f27af7279fe1da8e0d4666c1856843c298a14132
Hiroki Noda <kubo39@gmail.com>
2016-08-05 15:58:22 +0900

    Up.

--- example/source/app.d
+++ example/source/app.d
@@ -1,11 +1,12 @@
 import std.stdio;
 import std.json;
+import std.conv : to;
 import qiita : searchItems;
 
 void main()
 {
    auto resp = searchItems(["query": "dlang"]);
-   auto content = parseJSON(resp);
+   auto content = parseJSON(resp.to!string);
    foreach (i; 0 .. 5)
        content.array[i]["title"].str.writeln;
 }
